cb1ec8d60cf8533ead2c0ecb6540c69520c5962e
KorobovS <korobovs33@yandex.ru>
2025-01-09 09:44:01 +0300

    add Pojo classes

---
+++ src/test/java/ru/korobovs/pojo/RegisterBodyPojo.java
@@ -0,0 +1,26 @@
+package ru.korobovs.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RegisterBodyPojo {
+
+    private String email;
+    private String password;
+
+    public RegisterBodyPojo() {
+    }
+
+    public RegisterBodyPojo(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/test/java/ru/korobovs/pojo/UserBodyPojo.java
@@ -0,0 +1,26 @@
+package ru.korobovs.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserBodyPojo {
+
+    private String name;
+    private String job;
+
+    public UserBodyPojo() {
+    }
+
+    public UserBodyPojo(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+}
